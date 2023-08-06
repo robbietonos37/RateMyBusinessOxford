c5941281d3bc31c5ce80db0ae3d3cf5707f4e084
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-08-06 14:23:44 -0500

    Added function to render reviews

--- apartments.js
+++ apartments.js
@@ -1,14 +1,13 @@
 const toReviewButtons = document.querySelectorAll('.view-reviews')
 
-let head = 'bro fuck this shit'
 toReviewButtons.forEach(function (apt) {
     apt.addEventListener('click', (event) => {
         event.preventDefault()
-        let brobro = 'dude fuck you'
         location.assign('/view-reviews.html')
 
     })
 
 })
 
-head = brobro
+
+
--- view-reviews.js
+++ view-reviews.js
@@ -1,7 +1,20 @@
+const bizTitle = document.querySelector('#biz-title')
+const reviewsToView = document.querySelector('#reviews')
 
 const renderHead = function () {
-    const bizTitle = document.querySelector('#biz-title')
-    bizTitle.textContent = head
+    const reviews = getSavedRatings()
+    reviews.forEach((review) => {
+        let rating = review.ratingNumber
+        let text = review.reviewText
+        const ratingNumber = document.createElement('label')
+        ratingNumber.textContent = rating
+        const reviewText = document.createElement('p')
+        reviewText.textContent = text
+        reviewsToView.appendChild(ratingNumber)
+        reviewsToView.appendChild(reviewText)
+    })
+
+
 }
 
 renderHead()
